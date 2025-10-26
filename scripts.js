3893bd256a880fa285e922eb026d0dcdadba446e
Xurmayo <kizuki1337@gmail.com>
2025-10-26 12:20:44 +0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  console.log("jQuery is ready!");
+
+  // Example shared function
+  $(".btn-brand").click(function() {
+    console.log("Button clicked!");
+  });
+});
+
+
